0f202284d7d40fa8a6029951c76a229ec7d2b6df
Sammione <108240614+Sammione@users.noreply.github.com>
2025-11-13 12:24:51 +0100

    config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-BASE_URL = "https://sysprosystembackend-develop-hybyc7adhkh4cgfy.eastus-01.azurewebsites.net/"
-FAQ_ENDPOINT = "/api/v1/FNI"
+BASE_URL = "baseURL"
+FAQ_ENDPOINT = "EndPoint"
 
 def get_auth_headers(token: str):
     return {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json"
     }
+
 
 
